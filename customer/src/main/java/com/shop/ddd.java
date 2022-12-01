b2a593628e9729a37fec5e37b47271d0c46b2dbc
jeanm <jeanm@JEAN-PC>
2022-12-01 17:06:19 +0900

    1`

--- customer/src/main/java/com/shop/ddd.java
+++ customer/src/main/java/com/shop/ddd.java
@@ -5,4 +5,6 @@ public class ddd {
 	public void main() {}
 	public void aa() {}
 	public void ada() {}
+	public void aa1() {}
+	public void ada1() {}
 }
--- customer/src/main/java/com/shop/ddd2.java
+++ customer/src/main/java/com/shop/ddd2.java
@@ -4,4 +4,5 @@ public class ddd2 {
 	
 	public void main() {}
 	public void aa() {}
+	public void bbb() {}
 }
